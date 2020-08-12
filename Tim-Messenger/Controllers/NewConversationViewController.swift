2ff7b31c09595afd4e5a30afb081a2eee422397b
Tim Cho <timcho@umich.edu>
2020-08-12 21:52:15 +0800

    set up for files

---
+++ Tim-Messenger/Controllers/NewConversationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NewConversationViewController.swift
+//  Tim-Messenger
+//
+//  Created by Tim Cho on 12/8/2020.
+//  Copyright © 2020 Tim Cho. All rights reserved.
+//
+
+import UIKit
+
+class NewConversationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
