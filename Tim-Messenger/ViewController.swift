44ec5c02a74e6693ff7ea8bd2417fed83f9a0b8e
Tim Cho <timcho@umich.edu>
2020-08-12 21:24:38 +0800

    Initial Commit

---
+++ Tim-Messenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tim-Messenger
+//
+//  Created by Tim Cho on 12/8/2020.
+//  Copyright © 2020 Tim Cho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
